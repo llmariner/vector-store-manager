49b8b46dcc94f87bcb2301ff0272511fe68f2388
Aya Igarashi <ladiclexxx@gmail.com>
2024-10-09 16:52:46 +0900

    Configure release workflows

---
+++ hack/update-chart-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu -o pipefail
+
+ver=$1
+name=server
+
+echo "Update chart and app version for $name" \
+    && sed -i "s/^version:.*$/version: ${ver}/" deployments/$name/Chart.yaml \
+    && sed -i "s/^appVersion:.*$/appVersion: ${ver}/" deployments/$name/Chart.yaml \
+    && echo "=> done!"
